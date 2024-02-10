afe18608d5fa1547d4966904ae9f456c0b7850d0
Jesper Quorning <jesper.quorning@gmail.com>
2024-02-10 04:26:36 +0100

    Update adanow.rb

--- adanow.rb
+++ adanow.rb
@@ -1,32 +1,6 @@
-
 #!/usr/bin/env ruby
 #
 # license: MIT
-
-App     = "AdaNow"
-app     = App.downcase
-Install = "Install"
-install = Install.downcase
-Env     = "Environment"
-env     = Env.downcase
-
-CMD = [ :env, :inst ]
-Cmd = [ :env => "Environment", :inst => "Install" ]
-
-cmd = Cmd.each do | k, v |
-   k.to_s.downcase
-end
-
-puts cmd
-
-# environment = env
-
-doc_tldr = <<DOC_TLDR
- > #{app} #{env} setup
- > #{app} #{install} all
-or simply
- > #{app} setup
-DOC_TLDR
 
 doc_summary = <<DOC_SUMMARY
  > #{app} #{env} check
@@ -37,18 +11,35 @@ doc_summary = <<DOC_SUMMARY
  > #{app} #{install} Alire       -- #{Install} the Alire packaging tools
 DOC_SUMMARY
 
-puts ""
-puts "#{App} -- The Homebrew Ada installer for macOS                      "
-puts "--                                                                          "
-puts ""
-puts "tl;dr;                                                                      "
-puts "----                                                                        "
-puts doc_tldr
-puts
-puts "The not so short version"
-puts "----"
-puts doc_summary
-puts
-puts "Usage                                                                       "
-puts "----                                                                        "
-puts "   #{app} #{install} [ GNAT | GPRBuild | GNATStudio | Alire ]               "
+def cmd_check
+   # Check alr avaliability  
+end
+
+def show_versions
+   system alr --version
+   system gcc --version
+   system gnatmake --version
+   system gprbuild --version
+end
+
+def show_welcome_banner
+   puts "Welcome to the world of Ada/SPARK programming."
+   puts "More info https://ada-lang.io and https://alire.ada.dev."
+   puts "Start with a fresh alire crate: $ alr init --bin my_crate"
+end
+
+def main
+   install_alire
+   # creat directory for alire
+   # install alr at certain position
+   # put license file somewhere
+   # ensure alr is in PATH
+   install_toolchain
+   # run alr with install native toolchain command
+   show_versions
+   # show version of alr, gcc, gnatmake, gprbuild
+   show_welcome_banner
+   # show welcome text with url to how to get started
+end
+
+main
